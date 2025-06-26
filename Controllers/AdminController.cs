992a14fdb75b67d2e362c2b04df1884683ab2b01
oleghasigov3@gmail.com <oleghasigov3@gmail.com>
2025-06-26 23:08:19 +0300

    .

--- Controllers/AdminController.cs
+++ Controllers/AdminController.cs
@@ -4,7 +4,9 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace HotelApi.Controllers
 {
-
+    /// <summary>
+    /// Контроллер для Администратора (список всех номеров, записи бронирования и выселения, добавление номера, удаление номера)
+    /// </summary>
     [ApiController]
     [Route("[controller]")]
     public class AdminController : ControllerBase
@@ -16,24 +18,42 @@ namespace HotelApi.Controllers
             _admin = admin;
         }
 
+        /// <summary>
+        /// Вывод списка всех номеров
+        /// </summary>
+        /// <returns>Список номеров</returns>
         [HttpGet]
         public IEnumerable<Room> Get()
         {
             return _admin.GetAllRooms();
         }
 
+        /// <summary>
+        /// Вывод записей бронирования и выселения
+        /// </summary>
+        /// <returns>Список записей</returns>
         [HttpGet("Journal")]
         public IEnumerable<BookingHistory> GetJournal()
         {
             return _admin.GetJournal();
         }
 
+        /// <summary>
+        /// Добавление нового номера
+        /// </summary>
+        /// <param name="newRoom">Данные номера (Number, Cost, Class, Details, BookingStatus)</param>
+        /// <returns></returns>
         [HttpPost]
         public string Post(Room newRoom)
         {
             return _admin.AddRoom(newRoom);
         }
 
+        /// <summary>
+        /// Удаление номера
+        /// </summary>
+        /// <param name="roomNumber">Данные номера (Number, Cost, Class, Details, BookingStatus)</param>
+        /// <returns></returns>
         [HttpDelete("{roomNumber}")]
         public string Delete(int roomNumber)
         {
--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -4,6 +4,9 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace HotelApi.Controllers;
 
+/// <summary>
+/// Контроллер для работы с Пользователями (вывод свободных номеров, бронирование, выселение)
+/// </summary>
 [ApiController]
 [Route("[controller]")]
 public class UserController : ControllerBase
@@ -15,18 +18,34 @@ public class UserController : ControllerBase
         _user = user;
     }
 
+    /// <summary>
+    /// Вывод списка не забронированных номеров
+    /// </summary>
+    /// <returns>Список номеров</returns>
     [HttpGet]
     public IEnumerable<Room> Get()
     {
         return _user.GetAvailabelRooms();
     }
 
+    /// <summary>
+    /// Бронирование номера
+    /// </summary>
+    /// <param name="roomNumber">Номер апартаментов</param>
+    /// <param name="guestName">Имя гостя</param>
+    /// <returns></returns>
     [HttpPost("{roomNumber}/{guestName}")]
     public string PostBookARoom(int roomNumber, string guestName)
     {
         return _user.BookARoom(roomNumber, guestName);
     }
 
+    /// <summary>
+    /// Выселение из номера
+    /// </summary>
+    /// <param name="roomNumber">Номер апартаментов</param>
+    /// <param name="guestName">Имя гостя</param>
+    /// <returns></returns>
     [HttpPut("{roomNumber}/{guestName}")]
     public string Put(int roomNumber, string guestName)
     {
